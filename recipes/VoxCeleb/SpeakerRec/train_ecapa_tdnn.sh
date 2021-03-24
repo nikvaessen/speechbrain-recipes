12a68a93c8a4b8b7aaf97a50c9298ab6af84da84
Nik Vaessen <git@mail.vaessen.tech>
2021-03-24 16:34:12 +0100

    create slurmable script for ecepa_tdnn

---
+++ recipes/VoxCeleb/SpeakerRec/train_ecapa_tdnn.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --partition=csedu
+#SBATCH --account=nvaessen
+#SBATCH --time=72:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=6
+#SBATCH --mem 20G
+#SBATCH --gres=gpu:1
+#SBATCH -o results/slurm/%j.out
+#SBATCH -e results/slurm/%j.err
+#SBATCH --mail-user=nvaessen
+#SBATCH --mail-type=BEGIN,END,FAIL
+
+poetry run python train_speaker_embeddings.py hparams/train_ecapa_tdnn.yaml
