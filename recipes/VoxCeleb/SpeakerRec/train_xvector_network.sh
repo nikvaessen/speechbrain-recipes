05373f252db3842596069a889d41c2daba58187d
Nik Vaessen <git@mail.vaessen.tech>
2021-03-24 14:58:36 +0100

    add slurmable script, modify xvector hparams for cluster

---
+++ recipes/VoxCeleb/SpeakerRec/train_xvector_network.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --partition=csedu
+#SBATCH --account=nvaessen
+#SBATCH --time=72:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=6
+#SBATCH --mem 20G
+#SBATCH --gres=gpu:1
+#SBATCH -o /scratch-csedu/other/nik/slurm/%j.out
+#SBATCH -e /scratch-csedu/other/nik/slurm/%j.err
+#SBATCH --mail-user=nvaessen
+#SBATCH --mail-type=BEGIN,END,FAIL
+
+poetry run python train_speaker_embeddings.py hparams/train_x_vectors.yaml
